3989d941e11753f4b7301f4e9527a309fe161c3b
BzKevin <36381472+BzKevin@users.noreply.github.com>
2018-06-30 14:38:29 -0400

    Add files via upload
    Added "export to PS" functionality. Exports selected photos to specified directory and then opens them in Photoshop.

--- OpenInPS.lua
+++ OpenInPS.lua
@@ -16,7 +16,7 @@
   along with darktable.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 
---[[	Version 1.0.0     6/30/2018
+--[[	Version 1.1.0     6/30/2018
 
 This plugin adds the module "OpenInPS" to darktable's lighttable view
 
@@ -27,10 +27,14 @@ Photoshop
 ****How to use****
 Require this file from your luarc file, as with any other dt plug-in.
 On initial run, setup the Photoshop executable path via the settings > lua options dialog. Restart may be required.
-Select the photo you wish to open in Photoshop and press Open
+Select the photo(s) you wish to open in Photoshop and press Open from the Open in Photoshop box
 	RAW files will open in ACR inside Photoshop
 	JPG, Tif, etc will open as a new file inside PS
 	Since this is opening the file, not exporting and then opening, it will NOT have any of your Darktable edits
+Select the photo(s) you wish to export to Photoshop and select the desired destination folder, set the rest of 
+	the export settings as with any other export from darktable. Since this is "exporting" file will be converted
+	from RAW to your chosen format and darktable edits WILL carry over. Note the files created by this will NOT be
+	automatically deleted or cleaned up, ever.
 ]]
 
 local dt = require "darktable"
@@ -46,6 +50,9 @@ else
 end
 
 -- READ PREFERENCES --
+df.set_executable_path_preference("openinps", dt.preferences.read("module_OpenInPS", "bin_path", "string"))
+dt.print_log("OpenInPS - Executable Path Preference: "..df.get_executable_path_preference("openinps"))
+last_used_location = dt.preferences.read("module_OpenInPS", "export_path", "string")
 
 local function build_execute_command(cmd, args, file_list)
 	local result = false
@@ -60,11 +67,18 @@ local function build_execute_command(cmd, args, file_list)
 end
 
 -- FUNCTION --
-local function OpenInPS()
+local function truncate(x)
+      return x<0 and math.ceil(x) or math.floor(x)
+end 
+local function show_status(storage, image, format, filename, --Store: Called on each exported image
+  number, total, high_quality, extra_data)
+     dt.print('Export to Photoshop: '..tostring(truncate(number)).." / "..tostring(truncate(total)))   
+end
+
+local function OpenInPS() --OPEN in Photoshop 
 	dt.print_log("Opening in Photoshop")
 	dt.print("Opening In Photoshop")
-	df.set_executable_path_preference("openinps", dt.preferences.read("module_OpenInPS", "bin_path", "string"))
-	dt.print_log("Executable Path Preference: "..df.get_executable_path_preference("openinps"))
+
 	local PS_Path = df.check_if_bin_exists("openinps")
 	if not PS_Path then
 		dt.print_error("Photoshop not found")
@@ -86,6 +100,43 @@ local function OpenInPS()
 	dt.print("Opening in Photoshop")
 	resp = dt.control.execute(run_cmd)
 end
+local function ExportToPS(storage, image_table, extra_data) --EXPORT to Photoshop
+	dt.print_log("Exporting to Photoshop")
+	dt.print("Exporting to Photoshop")
+
+	local PS_Path = df.check_if_bin_exists("openinps")
+	if not PS_Path then
+		dt.print_error("Photoshop not found")
+		dt.print("ERROR - Photoshop not found")
+		return
+	end
+	
+	--Inits--
+	local image_path = ""
+	local images_to_open = ""
+	local chooser_path = file_chooser_button_path.value
+	dt.preferences.write("module_OpenInPS", "export_path", "string", chooser_path)
+	
+	
+	for image,temp_path in pairs(image_table) do
+		dt.print_log(image.filename)
+		new_path = chooser_path..os_path_seperator..image.filename
+		while df.check_if_file_exists(new_path) do
+			new_path = df.filename_increment(new_path)
+			-- limit to 99 more exports of the original export
+			if string.match(df.get_basename(new_path), "_(d-)$") == "99" then
+				break
+			end
+		end
+		result = df.file_move(temp_path, new_path)
+		images_to_open = images_to_open..new_path.." "
+	end
+	dt.print_log("Result: "..tostring(result))
+	run_cmd = build_execute_command(PS_Path, "" , images_to_open)
+	dt.print_log("ExportToPS run_cmd = "..run_cmd)
+	dt.print("Opening in Photoshop")
+	resp = dt.control.execute(run_cmd)
+end
 
 -- GUI --
 local executables = {"openinps"}
@@ -97,7 +148,7 @@ OpenInPS_btn_run = dt.new_widget("button"){
 	tooltip = "Opens selected image in Photoshop",
 	clicked_callback = function() OpenInPS() end
 	}
-dt.register_lib(
+dt.register_lib( --OpenInPS
 	"OpenInPS_Lib",	-- Module name
 	"Open In Photoshop",	-- name
 	true,	-- expandable
@@ -108,6 +159,25 @@ dt.register_lib(
 		OpenInPS_btn_run
 	}
 )
+
+file_chooser_button_path = dt.new_widget("file_chooser_button"){
+    title = 'Select export path',  -- The title of the window when choosing a file
+    value = last_used_location,
+	is_directory = true,             -- True if the file chooser button only allows directories to be selected
+    tooltip ='select the target directory for the image(s)',
+}
+dt.register_storage(
+	"ExportToPS_Storage", --Module name
+	"Export to Photoshop", --Name
+	show_status, --store: called once per exported image
+	ExportToPS,  --finalize:
+	nil, --supported: 
+	nil, --initialize: 
+	dt.new_widget("box"){
+		orientation = "vertical",
+		file_chooser_button_path
+		}
+	)
 
 -- PREFERENCES --
 executable = "hdrmerge"
